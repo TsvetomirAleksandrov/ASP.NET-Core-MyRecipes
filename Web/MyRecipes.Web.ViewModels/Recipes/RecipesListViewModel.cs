802cba7c9a71c80f68bcda16c8c3fe9027af2cdb
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-27 10:14:39 +0200

    Paging refactored as Partial View

--- Web/MyRecipes.Web.ViewModels/Recipes/RecipesListViewModel.cs
+++ Web/MyRecipes.Web.ViewModels/Recipes/RecipesListViewModel.cs
@@ -1,26 +1,9 @@
 ﻿namespace MyRecipes.Web.ViewModels.Recipes
 {
-    using System;
     using System.Collections.Generic;
 
-    public class RecipesListViewModel
+    public class RecipesListViewModel : PagingViewModel
     {
         public IEnumerable<RecipeInListViewModel> Recipes { get; set; }
-
-        public int PageNumber { get; set; }
-
-        public bool HasPreviousPage => this.PageNumber > 1;
-
-        public bool HasNextPage => this.PageNumber < this.PagesCount;
-
-        public int PreviousPageNumber => this.PageNumber - 1;
-
-        public int NextPageNumber => this.PageNumber + 1;
-
-        public int PagesCount => (int)Math.Ceiling((double)this.RecipesCount / this.ItemsPerPage);
-
-        public int RecipesCount { get; set; }
-
-        public int ItemsPerPage { get; set; }
     }
 }
