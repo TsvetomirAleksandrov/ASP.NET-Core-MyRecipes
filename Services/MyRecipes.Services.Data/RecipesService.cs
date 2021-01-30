9c4958281981bea001570a4aa3d1ff5a8fdde87d
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-30 16:12:22 +0200

    update

--- Services/MyRecipes.Services.Data/RecipesService.cs
+++ Services/MyRecipes.Services.Data/RecipesService.cs
@@ -15,14 +15,14 @@
     {
         private readonly string[] allowedExtensions = new[] { "jpg", "png", "gif" };
         private readonly IDeletableEntityRepository<Recipe> recipesRepository;
-        private readonly IDeletableEntityRepository<Ingredient> ingredientsRepository;
+        private readonly IDeletableEntityRepository<Ingredient> ingredientsRespository;
 
         public RecipesService(
             IDeletableEntityRepository<Recipe> recipesRepository,
-            IDeletableEntityRepository<Ingredient> ingredientsRepository)
+            IDeletableEntityRepository<Ingredient> ingredientsRespository)
         {
             this.recipesRepository = recipesRepository;
-            this.ingredientsRepository = ingredientsRepository;
+            this.ingredientsRespository = ingredientsRespository;
         }
 
         public async Task CreateAsync(CreateRecipeInputModel input, string userId, string imagePath)
@@ -40,25 +40,20 @@
 
             foreach (var inputIngredient in input.Ingredients)
             {
-                var ingredient = this.ingredientsRepository.All().FirstOrDefault(x => x.Name == inputIngredient.IngredientName);
+                var ingredient = this.ingredientsRespository.All().FirstOrDefault(x => x.Name == inputIngredient.IngredientName);
                 if (ingredient == null)
                 {
-                    ingredient = new Ingredient
-                    {
-                        Name = inputIngredient.IngredientName,
-                    };
-
-                    recipe.Ingredients.Add(new RecipeIngredient
-                    {
-                        Ingredient = ingredient,
-                        Quantity = inputIngredient.Quantity,
-                    });
+                    ingredient = new Ingredient { Name = inputIngredient.IngredientName };
                 }
 
-                await this.recipesRepository.AddAsync(recipe);
-                await this.recipesRepository.SaveChangesAsync();
+                recipe.Ingredients.Add(new RecipeIngredient
+                {
+                    Ingredient = ingredient,
+                    Quantity = inputIngredient.Quantity,
+                });
             }
 
+            // /wwwroot/images/recipes/jhdsi-343g3h453-=g34g.jpg
             Directory.CreateDirectory($"{imagePath}/recipes/");
             foreach (var image in input.Images)
             {
@@ -69,10 +64,10 @@
                 }
 
                 var dbImage = new Image
-                    {
-                        AddedByUserId = userId,
-                        Extension = extension,
-                    };
+                {
+                    AddedByUserId = userId,
+                    Extension = extension,
+                };
                 recipe.Images.Add(dbImage);
 
                 var physicalPath = $"{imagePath}/recipes/{dbImage.Id}.{extension}";
@@ -84,15 +79,19 @@
             await this.recipesRepository.SaveChangesAsync();
         }
 
+        public async Task DeleteAsync(int id)
+        {
+            var recipe = this.recipesRepository.All().FirstOrDefault(x => x.Id == id);
+            this.recipesRepository.Delete(recipe);
+            await this.recipesRepository.SaveChangesAsync();
+        }
+
         public IEnumerable<T> GetAll<T>(int page, int itemsPerPage = 12)
         {
             var recipes = this.recipesRepository.AllAsNoTracking()
-             .OrderByDescending(x => x.Id)
-             .Skip((page - 1) * itemsPerPage)
-             .Take(itemsPerPage)
-             .To<T>()
-             .ToList();
-
+                .OrderByDescending(x => x.Id)
+                .Skip((page - 1) * itemsPerPage).Take(itemsPerPage)
+                .To<T>().ToList();
             return recipes;
         }
 
@@ -105,9 +104,40 @@
             return recipe;
         }
 
+        public IEnumerable<T> GetByIngredients<T>(IEnumerable<int> ingredientIds)
+        {
+            var query = this.recipesRepository.All().AsQueryable();
+            foreach (var ingredientId in ingredientIds)
+            {
+                query = query.Where(x => x.Ingredients.Any(i => i.IngredientId == ingredientId));
+            }
+
+            return query.To<T>().ToList();
+        }
+
         public int GetCount()
         {
             return this.recipesRepository.All().Count();
         }
+
+        public IEnumerable<T> GetRandom<T>(int count)
+        {
+            return this.recipesRepository.All()
+                .OrderBy(x => Guid.NewGuid())
+                .Take(count)
+                .To<T>().ToList();
+        }
+
+        //public async Task UpdateAsync(int id, EditRecipeInputModel input)
+        //{
+        //    var recipes = this.recipesRepository.All().FirstOrDefault(x => x.Id == id);
+        //    recipes.Name = input.Name;
+        //    recipes.Instructions = input.Instructions;
+        //    recipes.CookingTime = TimeSpan.FromMinutes(input.CookingTime);
+        //    recipes.PreparationTime = TimeSpan.FromMinutes(input.PreparationTime);
+        //    recipes.PortionsCount = input.PortionsCount;
+        //    recipes.CategoryId = input.CategoryId;
+        //    await this.recipesRepository.SaveChangesAsync();
+        //}
     }
 }
