bbf88c62ff0068545253a1b062c819319da2784e
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-01-30 19:21:20 +0200

    VoteService implemented

--- Data/MyRecipes.Data/ApplicationDbContext.cs
+++ Data/MyRecipes.Data/ApplicationDbContext.cs
@@ -33,6 +33,8 @@
 
         public DbSet<Ingredient> Ingredients { get; set; }
 
+        public DbSet<Vote> Votes { get; set; }
+
         public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
 
         public override int SaveChanges() => this.SaveChanges(true);
---
+++ Services/MyRecipes.Services.Data/IVotesService.cs
@@ -0,0 +1,9 @@
+﻿namespace MyRecipes.Services.Data
+{
+    using System.Threading.Tasks;
+
+    public interface IVotesService
+    {
+        Task SetVoteAsync(int recipeId, string userId, byte value);
+    }
+}
