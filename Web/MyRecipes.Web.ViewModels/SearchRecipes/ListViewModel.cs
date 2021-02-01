fa16c40ec5e58492eed9a03c3ce2132f1886538d
alexandrovbg <cecko.aleksandrov@gmail.com>
2021-02-01 19:30:53 +0200

    Search implemented

---
+++ Web/MyRecipes.Web.ViewModels/SearchRecipes/ListViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyRecipes.Web.ViewModels.SearchRecipes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using MyRecipes.Web.ViewModels.Recipes;
+
+    public class ListViewModel
+    {
+        public IEnumerable<RecipeInListViewModel> Recipes { get; set; }
+    }
+}
---
+++ Web/MyRecipes.Web.ViewModels/SearchRecipes/SearchListInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyRecipes.Web.ViewModels.SearchRecipes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SearchListInputModel
+    {
+       public IEnumerable<int> Ingredients { get; set; }
+    }
+}
